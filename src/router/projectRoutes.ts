762730bbe460ce0ccb9104324ef2f674e3ac7340
Layla Rodas <rodas.layla@gmail.com>
2024-05-05 19:09:15 +0200

    validations middleware --- create project

--- src/router/projectRoutes.ts
+++ src/router/projectRoutes.ts
@@ -1,10 +1,19 @@
 import { Router } from "express";
+import { body } from 'express-validator'
 import { ProjectController } from "../controllers/ProjectController";
+import { handleInputErrors } from "../middleware/validation";
 
 
 const router = Router();
 
-router.post('/', ProjectController.createProject)
+router.post('/',
+    body('projectName').notEmpty().withMessage('The project name is required'),
+    body('clientName').notEmpty().withMessage('The client name is required'),
+    body('description').notEmpty().withMessage('The description is required'),
+    handleInputErrors,
+ProjectController.createProject)
+
+
 router.get('/', ProjectController.getAllProjects)
 
 
