42417c361a6887adc47bcff192c7deaaafff2580
Layla Rodas <rodas.layla@gmail.com>
2024-05-28 18:04:43 +0200

    validations and custom validations

--- src/router/authRoutes.ts
+++ src/router/authRoutes.ts
@@ -1,9 +1,24 @@
 import { Router } from "express";
+import { body } from "express-validator";
+import { AuthController } from "../controllers/AuthController";
+import { handleInputErrors } from "../middleware/validation";
 
 const router = Router()
 
-router.get('/', (req, res)=> {
-    res.send('from /api/auth')
-})
+router.post('/create-account', 
+    body('name')
+        .notEmpty().withMessage('Name cannot be empty'),
+    body('password')
+        .isLength({min: 8}).withMessage('The password is very short, minimum 8 characters'),
+    body('password_confirmation').custom((value, {req} )=>{
+        if (value !== req.body.password) {
+            throw new Error('Passwords are not the same')
+        }
+        return true
+    }),
+    body('email')
+        .isEmail().withMessage('Invalid Email'),
+    handleInputErrors,
+AuthController.createAccount)
 
 export default router
