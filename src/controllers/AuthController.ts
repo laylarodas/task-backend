3177df7ba39d9ca0850f9328ea414fd0771ec0a5
Layla Rodas <rodas.layla@gmail.com>
2024-05-29 22:49:11 +0200

    method create account

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,8 +1,16 @@
 
 import type { Request, Response } from 'express'
+import User from '../models/User'
 
 export class AuthController {
     static createAccount = async (req: Request, res: Response) => {
-        res.send('from /api/auth')
+        try {
+            const user = new User(req.body)
+            await user.save()
+
+            res.send('Account created, check your email to confirm')
+        } catch (error) {
+            res.status(500).json({error: 'There was an error'})
+        }
     }
 }
