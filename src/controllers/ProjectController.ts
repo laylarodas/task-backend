77556568316f4f0106aec1161ed91f9a88c9ef61
Layla Rodas <rodas.layla@gmail.com>
2024-06-16 23:36:56 +0200

    declare global user - select data to show

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -6,6 +6,8 @@ export class ProjectController {
     //static methods
     static createProject = async (req: Request, res: Response) => {
         const project = new Project(req.body)
+
+        console.log(req.user)
         try {
             await project.save()
             res.send('Project created successfully')
--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,4 +1,15 @@
 import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken'
+import User, { IUser } from "../models/User";
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: IUser
+        }
+    }
+
+}
 
 export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
     const bearer = req.headers.authorization
@@ -8,8 +19,23 @@ export const authenticate = async (req: Request, res: Response, next: NextFuncti
         res.status(401).json({ message: error.message })
     }
 
-    const token = bearer.split(' ')[1]
-    console.log(token)
+    const [, token] = bearer.split(' ')
+    
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        if(typeof decoded === 'object') {
+            const user = await User.findById(decoded.id).lean().select('_id name email')
+            if(user) {
+                req.user = user
+            }else{
+                res.status(401).json({ error: 'Invalid Token' })
+            }
+        }
+        
+    } catch (error) {
+        res.status(500).json({ error: 'Invalid Token' })
+    }
 
     next()
 }
