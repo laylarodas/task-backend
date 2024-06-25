b0df634e76fa39d41fb5cec1642565b0b906de0e
Layla Rodas <rodas.layla@gmail.com>
2024-06-25 18:47:54 +0200

    see project as a collaborator

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -26,7 +26,8 @@ export class ProjectController {
 
             const projects= await Project.find({
                 $or: [
-                    {manager: { $in: req.user._id }}
+                    {manager: { $in: req.user._id }},
+                    {team: { $in: req.user._id }}
                 ]
             })
 
@@ -47,7 +48,7 @@ export class ProjectController {
                 return res.status(404).json({error: error.message})
             }
 
-            if (project.manager.toString() !== req.user._id.toString()) {
+            if (project.manager.toString() !== req.user._id.toString() && !project.team.includes(req.user._id.toString())) {
                 const error = new Error('Invalid action');
                 return res.status(404).json({error: error.message})
             }
