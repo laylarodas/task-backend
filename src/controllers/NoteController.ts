e7a870c9532dfe50daa1d1971ab7351c555b06a4
Layla Rodas <rodas.layla@gmail.com>
2024-07-04 09:45:15 +0200

    save notes

--- src/controllers/NoteController.ts
+++ src/controllers/NoteController.ts
@@ -4,5 +4,21 @@ import Note, { INote } from "../models/Note";
 export class NoteController {
     static createNote = async (req: Request<{}, {}, INote>, res: Response) => {
         const { content } = req.body;
+        const note = new Note()
+        note.content = content;
+        note.createdBy = req.user._id;
+        note.task = req.task._id;
+
+        
+        
+        console.log(note)
+        req.task.notes.push(note._id);
+
+        try {
+            await Promise.allSettled([req.task.save(), note.save()]);
+            res.send('Note created successfully')
+        } catch (error) {
+            res.status(500).json({ error: 'Server error'})
+        }
     }
 }
--- src/models/Task.ts
+++ src/models/Task.ts
@@ -19,6 +19,7 @@ export interface ITask extends Document {
         user: Types.ObjectId,
         status: TaskStatus
     }[]
+    notes: Types.ObjectId[]
 }
 
 
@@ -54,6 +55,12 @@ export const TaskSchema: Schema = new Schema({
                 enum: Object.values(taskStatus),
                 default: taskStatus.PENDING
             }
+        }
+    ],
+    notes: [
+        {
+            type: Types.ObjectId,
+            ref: 'Note'
         }
     ]
 
