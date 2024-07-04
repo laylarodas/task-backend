1963d9177cf1f9512d92c274f8b02d07aa8456bf
Layla Rodas <rodas.layla@gmail.com>
2024-07-04 10:24:05 +0200

    delete notes

--- src/controllers/NoteController.ts
+++ src/controllers/NoteController.ts
@@ -1,5 +1,11 @@
 import type { Request, Response } from "express";
 import Note, { INote } from "../models/Note";
+import { Types } from "mongoose";
+
+
+type NoteParams = {
+    noteId: Types.ObjectId
+}
 
 export class NoteController {
     static createNote = async (req: Request<{}, {}, INote>, res: Response) => {
@@ -28,6 +34,29 @@ export class NoteController {
             res.json(notes)
         } catch (error) {
             res.status(500).json({ error: 'Server error'})
+        }
+    }
+
+    static deleteNote = async (req: Request<NoteParams>, res: Response) => {
+        const { noteId } = req.params;
+        const note = await Note.findById(noteId);
+
+        if(!note) {
+            const error = new Error('Note not found');
+            res.status(404).json({ error: error.message });
+        }
+
+        if(note.createdBy.toString() !== req.user._id.toString()) {
+            res.status(401).json({ error: 'You are not allowed to delete this note' });
+        }
+
+        req.task.notes = req.task.notes.filter(note => note.toString() !== noteId.toString());
+
+        try {
+            await Promise.allSettled([req.task.save(), note.deleteOne()]);
+            res.send({ message: 'Note deleted successfully' });
+        } catch (error) {
+            res.status(500).json({ error: 'There was an error' });
         }
     }
 }
--- src/router/projectRoutes.ts
+++ src/router/projectRoutes.ts
@@ -127,4 +127,10 @@ router.get('/:projectId/tasks/:taskId/notes',
     NoteController.getTaskNotes
 )
 
+router.delete('/:projectId/tasks/:taskId/notes/:noteId',
+    param('noteId').isMongoId().withMessage('invalid ID'),
+    handleInputErrors,
+    NoteController.deleteNote
+)
+
 export default router;
