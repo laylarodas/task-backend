5e89b2ffff5b71350cff3fe35629cb3f8e8d904c
Layla Rodas <rodas.layla@gmail.com>
2024-07-09 18:25:17 +0200

    middleware delete notes from task

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,4 +1,5 @@
 import mongoose, { Schema, Document, Types } from "mongoose";
+import Note from "./Note";
 
 const taskStatus = {
     PENDING: 'pending',
@@ -65,6 +66,15 @@ export const TaskSchema: Schema = new Schema({
     ]
 
 }, { timestamps: true })
+
+//middleware to delete all notes related to a task
+
+TaskSchema.pre('deleteOne', { document: true, query: false }, async function () {
+    const taskId = this._id
+    if (!taskId) return
+    await Note.deleteMany({ task: taskId })
+
+})
 
 
 const Task = mongoose.model<ITask>('Task', TaskSchema);
